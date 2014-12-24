daff5a20549eb029c0256f112fd22a4b3a115f02
srihari <sriharisundar95@gmail.com>
2014-12-24 13:07:10 +0530

    First commit

---
+++ raw_input.py
@@ -0,0 +1,5 @@
+quest = raw_input("What is your quest?")
+color = raw_input("What is your favorite color?")
+
+print "Ah, so your name is " + raw_input("What is your name?") +" your quest is %s, " \
+"and your favorite color is %s." % (quest, color)
